9730c488f895fc41d28d6e51be2028d99f0c7b9b
Peilin <pelai@paypal.com>
2019-11-03 12:54:26 -0800

    change

--- my-app/src/components/Presentational/MentorDisplay.js
+++ my-app/src/components/Presentational/MentorDisplay.js
@@ -8,7 +8,17 @@ const MentorDisplay = ({item}) => (
         <h4>Preferece Matching Percentage: {item[2].preference_Count}%</h4>
         <h4>Profile Matching Percentage: {item[3].profile_Count}%</h4>
         <p style={{ color: 'blue' }}>Matched Preference</p>
-
+        {
+            item[5].preferenceList.map((param, key) => (
+                <p>{param.key}: {param.value}</p>
+            ))
+        }
+        <p style={{ color: 'blue' }}>Matched Profile</p>
+        {
+            item[4].profileList.map((param, key) => (
+                <p>{param.key}: {param.value}</p>
+            ))
+        }
     </div>
 )
 
