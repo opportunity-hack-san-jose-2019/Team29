41f4d7d99bb75b05fe150b941633658f473c6a76
Peilin <pelai@paypal.com>
2019-11-03 12:36:31 -0800

    change

--- my-app/src/components/Presentational/MentorDisplay.js
+++ my-app/src/components/Presentational/MentorDisplay.js
@@ -3,28 +3,12 @@ import PropTypes from 'prop-types'
 
 const MentorDisplay = ({item}) => (
     <div>
-        <h3>{item.name}</h3>
-        <h4>Email: {item.email}</h4>
+        <h3>{item[1].name}</h3>
+        <h4>Email: {item[0].email}</h4>
+        <h4>Preferece Matching Percentage: {item[2].preference_Count}%</h4>
+        <h4>Profile Matching Percentage: {item[3].profile_Count}%</h4>
         <p style={{ color: 'blue' }}>Matched Preference</p>
-        {
-            item.pref.map((param, key) => (
-                <li
-                    key={key}
-                    role='presentation'>
-                    {param.key}: {param.value}
-                </li>
-            ))
-        }
-        <p style={{ color: 'blue' }}>Profile Preference</p>
-        {
-            item.profile.map((param, key) => (
-                <li
-                    key={key}
-                    role='presentation'>
-                    {param.key}: {param.value}
-                </li>
-            ))
-        }
+
     </div>
 )
 
