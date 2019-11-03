1f976b0380e241e1f404083100fef97938737d88
hannah <hang.ngopham@gmail.com>
2019-11-03 01:02:32 -0700

    Resolve conflicts 2

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import Login from './Login/Login';
+import Signup from './Signup/Signup';
+import HomeMentee from './HomeMentee/HomeMentee';
+import HomeMentor from './HomeMentor/HomeMentor';
+import MenteeProfile from './MenteeProfile/MenteeProfile';
+import UpdateProfileMentee from "./UpdateProfileMentee/UpdateProfileMentee";
 
 class Main extends Component {
     render(){
         return(
             <div>
                 <Route path="/login" component={Login}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/home_mentee" component={HomeMentee}/>
+                <Route path="/home_mentor" component={HomeMentor}/>
+                <Route path="/mentee_profile" component={MenteeProfile}/>
+                <Route path="/update_mentee_prof" component={UpdateProfileMentee}/>
             </div>
         )
     }
---
+++ my-app/src/components/Signup/Signup.js
@@ -0,0 +1,207 @@
+import React, { Component } from "react";
+import "../../App.css";
+import axios from "axios";
+import cookie from "react-cookies";
+import { Redirect } from "react-router";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { connect } from 'react-redux';
+import { signup } from "../../redux-files/action";
+import {hostedAddress} from "../../GlobalVar"
+
+let redirectVar = null;
+
+const options = ["Mentee", "Mentor"];
+
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            authFlag: false,
+            role: ""
+        };
+        this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
+        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+        this.roleChangeHandler = this.roleChangeHandler.bind(this);
+        this.submitSignup = this.submitSignup.bind(this);
+    }
+    componentWillMount() {
+        this.setState({
+            authFlag: false
+        });
+    }
+    usernameChangeHandler = e => {
+        this.setState({
+            username: e.target.value
+        });
+    };
+    roleChangeHandler = value => {
+        this.setState({
+            role: value
+        });
+        this.role.value = { value };
+    };
+
+    passwordChangeHandler = e => {
+        this.setState({
+            password: e.target.value
+        });
+    };
+    submitSignup = e => {
+        var headers = new Headers();
+        e.preventDefault();
+
+        const data = {
+            username: this.state.username,
+            password: this.state.password,
+            userOption: this.state.userOption,
+            firstName: this.firstName.value,
+            lastName: this.lastName.value
+        };
+        console.log('data.lastName ' + data.lastName);
+        console.log('data.firstName ' + data.firstName);
+        console.log(data.username);
+        console.log(data.userOption);
+
+        axios.defaults.withCredentials = true;//very imp
+        axios.post(hostedAddress+":3001/signup", data)
+            .then(response => {
+                console.log("Status Code : ", response);
+                if (response.status === 200 && response.data!="exists" && response.data!="error") {
+                    console.log("new account created-");
+                    console.log(cookie.load('new1'));
+                    localStorage.setItem('bearer-token',response.headers.authorization)
+                    this.setState({
+                        authFlag: true
+                    });
+                } else if (response.status === 201 && response.data!="exists" && response.data!="error") {
+                    console.log("new restaurant created-");
+                    console.log(cookie.load('cookie'));
+                    this.setState({
+                        authFlag: true
+                    });
+                }
+                else if(response.data=="exists")
+                {
+                    alert("There's already an account associated with this email-id :(");
+                    this.setState({
+                        authFlag: false
+                    });
+                }
+                else
+                {
+                    alert("Invalid");
+                }
+            })
+            .catch (response => {
+                    alert("Invalid");
+                    this.setState({
+                        authFlag: false
+                    });
+                }
+            )
+        // this.props.signup(data);
+        // For Redux just uncomment this line, comment the above lines and the rest is as it is.
+        // Refer the bhagwan chart for understanding the flow.
+
+    };
+    render() {
+        if(!cookie.load('cookie')){
+            redirectVar = <Redirect to= "/signup"/>
+        }
+        else if(cookie.load('cookie')=='mentor')
+        {
+            redirectVar = <Redirect to= "/home_mentor"/>
+        }
+        else if(cookie.load('cookie')=='mentee')
+        {
+            redirectVar = <Redirect to= "/home_mentee"/>
+        }
+        return (
+            <div>
+            {redirectVar}
+            <div class="container">
+            <div class="login-form">
+            <form onSubmit={this.submitSignup}>
+            <div class="main-div">
+            <div class="panel">
+            <h2>Sign up for Year Up 3M program</h2>
+        </div>
+        <div class="form-group">
+            <input
+        ref={ref => (this.firstName = ref)}
+        //   onChange={this.usernameChangeHandler}
+        type="text"
+    class="form-control"
+        name="firstName"
+        placeholder="First Name"
+        required
+        />
+        </div>
+
+        <div class="form-group">
+            <input
+        ref={ref => (this.lastName = ref)}
+        //   onChange={this.usernameChangeHandler}
+        type="text"
+    class="form-control"
+        name="lastName"
+        placeholder="Last Name"
+        required
+        />
+        </div>
+
+        <div class="form-group">
+            <input
+        onChange={this.usernameChangeHandler}
+        type="email"
+    class="form-control"
+        name="username"
+        placeholder="Email"
+        required
+        />
+        </div>
+        <div class="form-group">
+            <input
+        onChange={this.passwordChangeHandler}
+        type="password"
+    class="form-control"
+        name="password"
+        placeholder="Password"
+        required
+        />
+        </div>
+        <div class="form-group">
+            <Dropdown
+        ref={ref => (this.role = ref)}
+        options={options}
+        onChange={this.roleChangeHandler}
+        value={this.state.role}
+        placeholder="I'm a.."
+        required
+        />
+        </div>
+        <div>
+        <input
+        ref={ref => (this.submit = ref)}
+        //   onChange={this.usernameChangeHandler}
+        type="submit"
+    class="btn btn-primary"
+        value="Next"
+            />
+            {/* <button onClick={this.submitLogin} class="btn btn-primary">
+                  Sign Up
+                </button> */}
+            </div>
+            </div>
+            </form>
+            </div>
+            </div>
+            </div>
+    );
+    }
+}
+
+export default (Signup);
